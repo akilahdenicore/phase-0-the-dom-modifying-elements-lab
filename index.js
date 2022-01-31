47e107fd3fd3148819d1450c26d3ca6843931bbb
Akilah Snowden <akilahdsnowden@gmail.com>
2022-01-31 18:58:21 -0500

    assignment complete

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+var main = document.getElementById("main");
+main.remove();
+
+const newHeader = document.createElement('h1');
+newHeader.setAttribute("id", "victory");
+
+newHeader.innerHTML = "Akilah is the champion";
+document.body.append(newHeader);
