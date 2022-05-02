fca9c8a4a0719a69c9c4ff67553bf34a18a2a763
Akilah Snowden <akilahdsnowden@gmail.com>
2022-05-02 10:04:39 -0400

    commit tree a63895499434f453c16220d40b6749c2f692882c

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
-var main = document.getElementById("main");
-main.remove();
+/* First Test:
+no longer has DOM node 'main#main':
+     AssertionError: Make sure you remove the <main> with id 'main': 
+     expected <main id="main"></main> to not exist
+     expect(document.querySelector('main')
+*/
 
-const newHeader = document.createElement('h1');
-newHeader.setAttribute("id", "victory");
+const element = document.getElementById('main');//grab element by id 
+element.remove(); //call remove on element
 
-newHeader.innerHTML = "Akilah is the champion";
-document.body.append(newHeader);
+/* Second Test:
+has a 'newHeader' variable that points to node 'h1#victory':
+ --> ReferenceError: newHeader is not defined
+    expect(newHeader.id, "Make sure you create an <h1> with id 'victory'").eql('victory')
+*/
+
+//create new element by grabbing document and declaring newHeader
+const newHeader = document.createElement('h1'); 
+//grab newHeader element and set id, passing in 'id' and id name 'victory'
+newHeader.setAttribute('id', 'victory'); 
+
+/* Third Test:
+has a 'newHeader' variable that points to node 'h1#victory' with "YOUR-NAME is the champion" inside:
+     AssertionError: Make sure you create an <h1> with id 'victory' with a sweet message in it:
+      expected '' to include 'is the champion'
+      expect(newHeader.innerHTML
+*/
+newHeader.innerHTML = 'My kitty Mimi is the champion';
